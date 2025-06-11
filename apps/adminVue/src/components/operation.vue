1283b326ceafc15775a38b882376e7c854c99ac2
杨家华 <karl20888@live.cn>
2025-06-12 01:36:41 +0800

    update

---
+++ apps/adminVue/src/components/operation.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import {
+  inject,
+  ref,
+  computed,
+  watch,
+} from 'vue';
+import { useVModels } from '@vueuse/core';
+
+const editor = inject('editor');
+const selected = inject('selected');
+
+const props = defineProps<{
+  intersection: boolean;
+}>();
+
+const emit = defineEmits<{
+  'update:intersection': [intersection: boolean];
+}>();
+
+const {
+  intersection,
+} = useVModels(props, emit);
+</script>
+
+<template>
+  <el-form v-if="!selected" size="small">
+    <el-form-item label="框选方式">
+      <el-radio-group v-model="intersection">
+        <el-radio-button :value="true">相交选择</el-radio-button>
+        <el-radio-button :value="false">包含选择</el-radio-button>
+      </el-radio-group>
+    </el-form-item>
+  </el-form>
+</template>
