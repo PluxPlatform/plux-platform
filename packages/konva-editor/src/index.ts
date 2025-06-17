71282c3e3381aa12dcdda190236bec0128b56dec
bhabgs <bhabgs@aliyun.com>
2025-06-17 10:56:35 +0800

    refactor(konva-editor): 重新组织导出语句顺序
    将默认导出 Editor 的语句移至文件末尾，保持导出语句的一致性

--- packages/konva-editor/src/index.ts
+++ packages/konva-editor/src/index.ts
@@ -1,12 +1,8 @@
 import Editor from "./editor";
 
-export default Editor;
-
 export * from "./uuid";
-
 export type * from "./types";
 export type * from "./types/enums";
-
 export * from "./node";
 export * from "./group";
 export * from "./rect";
@@ -14,3 +10,5 @@ export * from "./text";
 export * from "./circle";
 export * from "./button";
 export * from "./line";
+
+export default Editor;
