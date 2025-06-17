38817a64da19e23d0b7e36e0d650efc0df681378
bhabgs <bhabgs@aliyun.com>
2025-06-17 10:50:03 +0800

    fix: 解决合并冲突并统一导出语句格式

--- packages/konva-editor/src/index.ts
+++ packages/konva-editor/src/index.ts
@@ -7,20 +7,10 @@ export * from "./uuid";
 export type * from "./types";
 export type * from "./types/enums";
 
-<<<<<<< HEAD
 export * from "./node";
 export * from "./group";
 export * from "./rect";
 export * from "./text";
 export * from "./circle";
 export * from "./button";
-export * from "./elements/Tag";
-=======
-export * from './node';
-export * from './group';
-export * from './rect';
-export * from './text';
-export * from './circle';
-export * from './button';
-export * from './line';
->>>>>>> ee/main
+export * from "./line";
